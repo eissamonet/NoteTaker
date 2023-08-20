c89d8eb4171d30e8fd3213427e5d0535ebab33dd
Eissa Purnell <eissamonet@gmail.com>
2023-08-20 13:34:54 -0500

    changes to files

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes.js');
-const htmlRoutes = require('./routes/htmlRoutes.js');
+const index = require('./routes/index.js')
+const notes = require('./routes/notes.js');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -9,10 +9,10 @@ const app = express();
 //parse incoming string
 app.use(express.urlencoded({ extended: true }));
 
-// parse json data
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', index);
+app.use('/', notes);
 
 app.use(express.static('public'));
 
