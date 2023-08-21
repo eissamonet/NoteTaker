4d41d8df7fbcb14dfa8a762f797ecdaa13a208a6
Eissa Purnell <eissamonet@gmail.com>
2023-08-21 15:44:26 -0500

    middleware + clog file added

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 const htmlRoutes = require('./routes/htmlRoutes.js')
 const apiRoutes = require('./routes/apiRoutes.js');
+
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
+// Import custom middleware, "cLog"
+app.use(clog);
 
-//parse url encoded and json
+
+//middleware for parsing url encoded and json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
