12b208c2316f6cc4c46fef66faa251d45cc72dbf
Eissa Purnell <eissamonet@gmail.com>
2023-11-04 14:10:34 -0500

    changes made to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,25 @@
+// dependencies
 const express = require('express');
-const path = require('path');
-const { clog } = require('./middleware/clog.js');
-const index = require('./routes/index.js')
-const notes = require('./routes/notes.js');
 
-const PORT = process.env.PORT || 3001;
+// server to use routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+// creat server
 const app = express();
 
-// Import custom middleware, "clog"
-app.use(clog);
-
+// set port
+const PORT = process.env.PORT || 3001;
 
 //middleware for parsing url encoded and json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //use routes
-app.use('/api', notes);
-app.use('/', index);
-
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
